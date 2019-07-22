823fb9aa3847e78ce9516b19a175618df2787648
Sapir Malka <sapirm290@gmail.com>
2019-07-22 09:43:12 +0300

    Update geek1.sql

--- sql-basics/geek1.sql
+++ sql-basics/geek1.sql
@@ -1,9 +1,11 @@
-use employees;
-show tables;
-select * from current_dept_emp;
+SELECT 
+    YEAR(hire_date) AS y, COUNT(*)
+FROM
+    employees
+GROUP BY y
+ORDER BY y;
 
 
-select year(hire_date) as y, count(*) from employees group by y order by y;
 SELECT 
     YEAR(hire_date), MONTH(hire_date), COUNT(*)
 FROM
@@ -12,25 +14,59 @@ GROUP BY MONTH(e.hire_date) , YEAR(e.hire_date)
 ORDER BY hire_date ASC;
 
 
-select  emp_no as e, avg(salary) from salaries group by e;
-select count(distinct title) from titles;
-select title, count(*) from titles group by title;
-select  emp_no as e, sum(salary) from salaries group by e;
-select count(dept_no) as d from dept_emp group by emp_no order by d desc limit 1;
+SELECT 
+    dept_no, COUNT(emp_no)
+FROM
+    dept_emp
+GROUP BY dept_no;
 
 
--- select count(*) from dept_emp where year(hire_date) >  group by dept_no, year(hire_date)
--- select count(*),  year(hire_date) from employees group by year(hire_date);
--- distinct month(hire_date),
+SELECT 
+    emp_no AS e, AVG(salary)
+FROM
+    salaries
+GROUP BY e;
 
 
--- 1. What is the number of employees hired each year?
--- 2. What is the number of employees hired each month per each year?
--- 3. How many employees were in each department per year?
--- 4. What is the average salary each employee had in their time working for the company?
--- 5. How many different titles are there in the company?
--- 6. How many people hold every different title?
--- 7. How much money did every employee make in the company?
--- 8. What is the maximum amount of departments a single employee worked at?
--- 9. How many male/female employees were there in the company?
--- 10. Who are the 10 youngest employees in the company?
+SELECT 
+    COUNT(DISTINCT title)
+FROM
+    titles;
+    
+    
+SELECT 
+    title, COUNT(*)
+FROM
+    titles
+GROUP BY title;
+
+
+SELECT 
+    emp_no AS e, SUM(salary)
+FROM
+    salaries
+GROUP BY e;
+
+
+SELECT 
+    COUNT(dept_no) AS d
+FROM
+    dept_emp
+GROUP BY emp_no
+ORDER BY d DESC
+LIMIT 1;
+
+
+SELECT 
+    gender, COUNT(*)
+FROM
+    employees
+GROUP BY gender;
+
+
+SELECT 
+    emp_no, birth_date
+FROM
+    employees
+ORDER BY birth_date DESC
+LIMIT 10;
